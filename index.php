e906db18b4f8b80054f3b0a3e5069842056db93e
FerhatKortak <ferhat_ultra_@hotmail.com>
2014-09-24 02:57:28 +0300

    Draft-1
    Files created and written some codes.

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ferhat Kortak - Blog</title>
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+<div id="container">
+	<div id="leftbio">
+		<table id="tblprofile">
+		<!-- Name and Surname -->
+			<tr>
+				<td></td>
+			</tr>
+		<!-- Picture/Logo -->
+			<tr>
+				<td></td>
+			</tr>
+		<!-- Name and Surname -->
+			<tr>
+				<td></td>
+			</tr>
+		<!-- Social Media Buttons -->
+			<tr>
+				<td></td>
+			</tr>
+		</table>
+	</div>
+	<div id="rightcontent">
+		<!-- Articles... -->
+	</div> 
+</div>
+</body>
+</html>
